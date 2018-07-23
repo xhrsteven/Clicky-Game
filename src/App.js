ddd87efbebd2dfddb53a985b456c76c584971a8d
Steven <xhrsteven@gmail.com>
2018-07-22 21:06:13 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -7,28 +7,32 @@ import Title from './component/Title';
 import './App.css'
 import GridList from '@material-ui/core/GridList';
 
+function shuffleFriends(friend){
 
+    let i = friends.length -1;
+    while(i >0){
+        const j = Math.floor(Math.random()*(i+1));
+        const p = friends[i];
+        friends[i] = friends[j];
+        friends[j] =p;
+        i --;
+    }
+    return friends
+}
 class App extends Component {
 
     state = {
         friends,
-        currentScore : 0,
+        currentScore: 0,
         topScore: 0,
         message: 'Click Picture You like',
-        clicked: false,
+        clicked: [],
     }
 
-    shuffleArrary = array =>{
-        for (let i = array.length - 1; i > 0; i--) {
-          let j = Math.floor(Math.random() * (i + 1));
-          [array[i], array[j]] = [array[j], array[i]];
-        }
-    }
-
-    handleClick = () =>{
-        if(this.state.clicked === false){
+    handleClick = id => {
+        if(this.state.clicked.indexOf(id) === -1 ){
             this.handleIncrement();
-            this.setState({clicked : true});
+            this.setState({clicked : this.state.clicked.concat(id)});
         }else{
             this.handleReset();
         }
@@ -39,13 +43,14 @@ class App extends Component {
         this.setState({
             currentScore: newScore,
             message: 'Keep Moving!'
-        })
+        });
         if(newScore >= this.state.topScore){
             this.setState({topScore: newScore});
         }
         else if (newScore === 12){
             this.setState({message: 'Winner Winner Chicken Dinner!'})
         } 
+        this.handleShuffle();
     };
 
     handleReset = () =>{
@@ -53,13 +58,13 @@ class App extends Component {
             currentScore: 0,
             topScore: this.state.topScore,
             message: 'Try Again!',
-            clicked: false,
+            clicked: [],
         });
         this.handleShuffle();
     };
 
-    handleShuffle = ()=>{
-        let shuffleFriend = this.shuffleArrary(friends);
+    handleShuffle = (friend)=>{
+        let shuffleFriend = shuffleFriends(friend);
         this.setState({friends: shuffleFriend});
     };
 
@@ -67,7 +72,7 @@ class App extends Component {
         return (
             <Fragment>
                 <NavBar
-                    score = {this.state.currentScore}
+                    currentScore = {this.state.currentScore}
                     topScore = {this.state.topScore}
                     message = {this.state.message}
                 />
@@ -77,7 +82,7 @@ class App extends Component {
                 <GridList item cols={3}>
                     {this.state.friends.map(friend =>(
                             <FriendCard
-                                key={friend}
+                                key={friend.id}
                                 handleClick= {this.handleClick}
                                 handleIncrement={this.handleIncrement}
                                 handleReset={this.handleReset}
