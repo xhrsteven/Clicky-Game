3388e9187ccac1c0b0fa6fbda25e970d0b3e8e55
Steven <xhrsteven@gmail.com>
2018-07-23 15:06:16 -0500

    Update Webpack

---
+++ config/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename:'./index.html'
+})
+
+module.exports = {
+  // entry: {index: '/src/index.html'},
+  // output: {
+  //   path: path.resolve("dist"),
+  //   filename: "bundled.js"
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
